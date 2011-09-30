934d110f1edf20879d6be566eea968b1cf81d12b
Florent Galland <florent.galland@gmail.com>
2011-09-30 15:51:02 +0200

    Continue dev. getCurrentLocation not working very well for the moment.

--- context.py
+++ context.py
@@ -1,30 +1,35 @@
 import time
-#import android
-#droid = android.Android()
+import math
+import android
 
+# ---------------------------
 class RuleManager:
-    """The context class provides definitions of rules
-    and events which are actions to be realized in specific
-    contexts of time and location. """
+    """
+    The RuleManager class provides services to handle the user defined rules
+    which are actions to be realized in specific contexts of time and location.
+    """
     def __init__(self):
         self._rules = []
-        self._events = []
 
     def addRule(self, rule):
         self._rules.append(rule)
+        print self._rules
 
     def followRules(self):
+        global androidHelper
+        
         for rule in self._rules:
             performAction = True
             contexts = rule['context']
+            
             # Compare current context and rule context:
             if 'time' in contexts:
                 if not (time.localtime().tm_hour == contexts['time'][0] and time.localtime().tm_min == contexts['time'][1]):
                     performAction = False
 
-            currentLocation = 'ici'
             if 'location' in contexts:
-                if not currentLocation == contexts['location']:
+                currentLocation = androidHelper.getCurrentLocation()
+                if not self.areSameLocations(contexts['location'], currentLocation):
                     performAction = False
 
             if performAction:
@@ -32,28 +37,79 @@ class RuleManager:
                 rule['action']()
             else:
                 print 'Not doing anything.'
-                
-# ---------------------------
-def toast():
-    #droid.makeToast('salut')
-    #droid.speak('salut')
-    print 'Toasting.'
-    
+
+    def areSameLocations(self, location1, location2):
+        """
+        using the spherical law of cosines from 
+        http://www.movable-type.co.uk/scripts/latlong.html
+        this law gives the distance in kilometers.
+        """
+        R = 6371
+        lat1 = math.radians(location1['latitude'])
+        lat2 = math.radians(location2['latitude'])
+        lon1 = math.radians(location1['longitude'])
+        lon2 = math.radians(location2['longitude'])
+        dist = math.acos(math.sin(lat1)*math.sin(lat2) +
+                          (math.cos(lat1)*math.cos(lat2) *
+                           math.cos(lon2-lon1))) * R
+        tolerance = 2
+        return (dist < 2)
 
 # ---------------------------
+class AndroidHelper:
+    """
+    The AndroidHelper class provides some helping functions
+    for the Android Facade API.
+    """
+    def __init__(self):
+        self._droid = android.Android()
+
+    def getCurrentLocation(self):
+        self._droid.startLocating()
+        time.sleep(2)
+        loc = self._droid.readLocation()
+        while len(loc.result) == 0:
+            print loc
+            time.sleep(1)
+            loc = self._droid.readLocation()
+
+        self._droid.stopLocating()
+
+        print loc.result['network']['latitude']
+        print loc.result['network']['longitude']
+        
+        return {
+            'latitude': loc.result['network']['latitude'],
+            'longitude': loc.result['network']['longitude']}
+
+    def makeToast(self, message):
+        self._droid.makeToast(message)
+        print 'Toasting.'
+
+# ---------------------------
+# Available actions:
+def toast():
+    androidHelper = AndroidHelper()
+    androidHelper.makeToast('toasting')
+
+# ---------------------------
+androidHelper = AndroidHelper()
 ruleMgr = RuleManager()
+
 ruleMgr.addRule({
     'context': {
-        'time': (15,33),
-        'location': 'ici'
-    },
-    'action': toast
+        'time': (15,26),
+        'location': {
+            'latitude': 45.7797498,
+            'longitude':  4.8770637
+            }
+        },
+        'action': toast
 })
 
 ruleMgr.addRule({
     'context': {
         'time': (14,22),
-        'location': 'travail'
     },
     'action': toast
 })
